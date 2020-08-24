54006a92438468d2df4615f9bb80cce1d85d9dca
Dhruv Batheja <dhruv.batheja@booking.com>
2020-08-24 02:43:00 +0200

    Added basic polling and json fetch

--- static/app.js
+++ static/app.js
@@ -6,28 +6,29 @@ const projection = d3.geoMercator().fitSize([width, height], geoJSON);
 
 const geoGenerator = d3.geoPath(projection);
 
-
-const svg = d3.create("svg")
+// TODO: Ugly svg refresh. Improve this logic
+refresh = ()=>{
+    item = document.getElementById("svgd3");
+    if (item != null) {
+        item.remove();
+    }
+    const svg = d3.create("svg")
+    .attr("id", "svgd3")
     .attr("width", width)
     .attr("height", height)
     .attr("viewbox", [0, 0, width, height])
     .attr("style", "background-color: #02101b");
-
-
-svg.append("path")
+    svg.append("path")
     .datum(geoJSON)
     .attr("d", geoGenerator)
     .attr("fill", "#2a2b28")
     .attr("stroke", "none");
-
-
-svg.append("path")
+    svg.append("path")
     .datum(traffic)
     .attr("d", geoGenerator)
     .attr("fill", "red")
     .attr("stroke", "none");
-
-    
-
-document.body.append(svg.node())
-
+    document.body.append(svg.node());
+}
+refresh()
+setInterval(refresh, 1000)
