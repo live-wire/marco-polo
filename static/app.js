76394172ba2ad2017d79a485f3755706114ddcc5
mohinderps <mohin19derp@gmail.com>
2020-10-11 14:09:26 +0530

    Changes map design and adds traffic transition

--- static/app.js
+++ static/app.js
@@ -12,11 +12,11 @@ const svg = d3.select("body")
     .attr("width", width)
     .attr("height", height)
     .attr("viewBox", [150, 0, width / 1.25, height / 1.25 ])
-    .style("background-color", "#02101b")
+    .style("background-color", "#030F1E")
 
 const countries = svg.append("g")
-    .attr("fill", "#2a2b28")
-    .attr("stroke", "none")
+    .attr("fill", "#142E50")
+    .attr("stroke", "#3D6298")
     .attr("cursor", "pointer")
     .selectAll("path")
     .data(geoJSON.features)
@@ -27,18 +27,28 @@ countries.append("title")
     .text(d => d.properties["NAME"])
 
 const traffic = svg.append("g")
-    .attr("fill", "red")
+    .attr("fill", "rgba(26, 115, 232)")
     .attr("stroke", "none")
     .attr("cursor", "pointer");
 
 
 setInterval(() => {
+    const t = svg.transition().duration(1000);
+
+
     fetchLiveTraffic().then(d=>{
         const data = d["default"]; // TODO: hardcoded default here (Use the selected service 
 
         traffic.selectAll("path")
-            .data(data)
-            .join("path")
+            .data(data, data => data.properties.ip)
+            .join(enter => enter.append("path")
+                .attr("opacity", 0)
+                .call(enter => enter.transition(t).attr("opacity", 1)), 
+                update => update,
+                exit => exit.call(exit => exit.transition(t)
+                    .attr("opacity", 0)
+                    .remove())
+            )
             .attr("d", geoGenerator)
             .selectAll("title")
             .data((d, i) => data.slice(i, i+1))
